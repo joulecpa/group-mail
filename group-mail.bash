b1e71eea33d1a6c747b3dc6a06009272e882f757
root <root@rome.(none)>
2014-07-24 10:25:29 -0400

    Updated unsubscribe functionality

--- group-mail.bash
+++ group-mail.bash
@@ -8,15 +8,15 @@ body=$(/bin/cat body.txt) # Reduce disk access time
 recipients=$(/usr/bin/sqlite3 fsosscfp.sqlite3 "SELECT * FROM FSOSSCFP")
 
 while read -r recipient; do
-read email id <<< "$recipient"
+	read email id <<< "$recipient"
 
-
-/usr/sbin/sendmail $email <<-EOF
-subject:Seneca FSOSS 2014 - Call for Presentation Proposals
-from:FSOSS-CFP <fsoss-cfp@senecacollege.ca>
-Content-Type: text/html
-MIME-Version: 1.0"
-$body
-<p>If you do not wish to receive further email regarding FSOSS, please <a href="http://rome.proximity.on.ca/unsubscribe.php?email=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$2")&id=$id">click here to unsubscribe</a>.</p>
-EOF
+	/usr/sbin/sendmail $email <<-EOF
+		to:$email
+		subject:Seneca FSOSS 2014 - Call for Presentation Proposals
+		from:FSOSS-CFP <fsoss-cfp@senecac.on.ca>
+		Content-Type: text/html
+		MIME-Version: 1.0"
+		$body
+		<p>If you do not wish to receive further email regarding FSOSS, please <a href="http://rome.proximity.on.ca/fsoss/unsubscribe.php?email=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$email")&id=$id">click here to unsubscribe</a>.</p>
+	EOF
 done <<< "$recipients"
--- unsubscribe.php
+++ unsubscribe.php
@@ -1,36 +1,33 @@
-#!/usr/bin/env php
-
-# Remove email address entry from a CSV file
-# Author: Christopher Markieta
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>FSOSS Unsubscribe</title>
 </head>
-
 <body>
 
 <?php
+// #!/usr/bin/env php
+
+// Remove email address entry from a CSV file
+// Author: Christopher Markieta
+
 try
 {
     $email = SQLite3::escapeString(urldecode($_GET['email']));
     $id    = SQLite3::escapeString($_GET['id']);
-    $db    = new PDO('/home/backup/group-mail/fsosscfp.sqlite3') or die("cannot open the database");
+    $db    = new PDO('sqlite:/var/fsoss/fsosscfp.sqlite3') or die ('cannot open the database');
 
-    $stmt = $db->prepare('DELETE FROM FSOSSCFP WHERE EMAIL=:email AND ID=:id');
-    $stmt->bindValue(':email', $email, SQLITE3_TEXT);
-    $stmt->bindValue(':id',    $id,    SQLITE3_INTEGER);
-    $result = $stmt->execute();
+    $stmt = $db->prepare("DELETE FROM FSOSSCFP WHERE EMAIL=:email AND ID=:id");
+    $stmt->execute(array(":email" => $email, ":id" => $id));
     $db     = null;
 ?>
     <h3>Thank You</h3>
     <p>You have been unsubscribed from the mailing list.</p>
 <?php }
-catch
+catch(Exception $e)
 { ?>
-    <h3>Something went wrong!</h3>
-    <p>Please send us an email at <a href="mailto:ostep-team@senecac.on.ca"?Subject=FSOSS%20Unsubscribe%20Failure>ostep-team@senecac.on.ca</a></p>
+    <h3>Something went wrong</h3>
+    <p>Please send us an email at <a href="mailto:ostep-team@senecac.on.ca?Subject=FSOSS%20Unsubscribe%20Failed">ostep-team@senecac.on.ca</a> .</p>
 <?php } ?>
 
 </body>
