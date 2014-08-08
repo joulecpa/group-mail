32b8d744988e29ac6a864cc75c3a502cb08e1b23
root <root@rome.(none)>
2014-08-08 18:20:02 -0400

    Added footer for embedding images

--- group-mail.bash
+++ group-mail.bash
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
-# Send email to CSV list of addresses
+# Send email to database list of addresses
 # Author: Christopher Markieta
 
-IFS='|'
-body=$(/bin/cat body.txt) # Reduce disk access time
-recipients=$(/usr/bin/sqlite3 fsosscfp.sqlite3 "SELECT * FROM FSOSSCFP")
+if [ -z "$1" ]; then
+	echo 'Missing argument for database file'
+else
+	IFS='|'
+	body=$(/bin/cat body.html) # Reduce disk access time
+	footer=$(/bin/cat footer.html)
+	recipients=$(/usr/bin/sqlite3 "$1" "SELECT * FROM FSOSSCFP")
 
-while read -r recipient; do
-	read email id <<< "$recipient"
+	while read -r recipient; do
+		read email id <<< "$recipient"
 
-	/usr/sbin/sendmail $email <<-EOF
-		to:$email
-		subject:Seneca FSOSS 2014 - Call for Presentation Proposals
-		from:FSOSS-CFP <fsoss-cfp@senecac.on.ca>
-		Content-Type: text/html
-		MIME-Version: 1.0"
-		$body
-		<p>If you do not wish to receive further email regarding FSOSS, please <a href="http://rome.proximity.on.ca/fsoss/unsubscribe.php?email=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$email")&id=$id">click here to unsubscribe</a>.</p>
-	EOF
-done <<< "$recipients"
+		/usr/sbin/sendmail $email <<-EOF
+			to:$email
+			subject:FSOSS 2014 Evite (UPDATED FINAL - READY FOR USE)
+			from:FSOSS-CFP <fsoss-cfp@senecac.on.ca>
+			$body
+			<div align=3D"center">If you do not wish to receive further email regarding=
+			 FSOSS, please <a href=3D"http://rome.proximity.on.ca/fsoss/unsubscribe.php=
+			?email=3D$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$email")&id=
+			=3D$id">click here to unsubscribe</a></div>
+			$footer
+		EOF
+	done <<< "$recipients"
+fi
